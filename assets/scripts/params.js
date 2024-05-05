92080708014b9873b333a231f0df753ff1e0e96b
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-05-05 03:37:19 +0300

    Implement pages

---
+++ assets/scripts/params.js
@@ -0,0 +1,9 @@
+window.RufflePlayer = window.RufflePlayer || {};
+RufflePlayer.config = RufflePlayer.config || {};
+RufflePlayer.config = {
+    quality: 'best',
+    logLevel: 'error',
+    splashScreen: false,
+    autoplay: 'on',
+    unmuteOverlay: 'hidden',
+};
